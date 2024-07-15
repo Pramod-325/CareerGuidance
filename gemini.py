988e1b7970fb4a8a5c548a806da29d5abeec8f0c
pramod-325 <vpramodkumar2003@gmail.com>
2024-07-15 20:14:18 +0530

    env updated for streamlit deploy

--- gemini.py
+++ gemini.py
@@ -2,7 +2,7 @@ import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 # Creating the model
 generation_config = {
